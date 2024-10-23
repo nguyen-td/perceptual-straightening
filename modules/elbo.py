31290ba4404cfa42684011adf927a6bce9b7f66c
Tien Dung Nguyen <nguyen.tiendung.otten@gmail.com>
2024-10-23 18:30:32 -0500

    update comment

--- modules/elbo.py
+++ modules/elbo.py
@@ -298,7 +298,7 @@ class ELBO(nn.Module):
     
     def compute_loss(self, log_ll, kl_divergence):
         """
-        Returns the ELBO function. Returns the negative function because the optimizers minimize.
+        Returns the ELBO function. Minimizing the negative log likelihood is equivalent to maximizing the likelihood.
 
         Inputs:
         -------
